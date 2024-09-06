b96e9515d8fa1f7ddf47b3471958dd5af1f0b328
devMachadoA <amachadoamaral2@gmail.com>
2024-09-06 20:35:45 +0100

    finished code

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import { DataProvider } from './context/DataContext';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/page/HomePage';
 import Details from './components/page/Details';
-import Header from './components/header/Header';
+import Layout from './components/Layout/Layout';
 
 import './App.css';
-import Layout from './components/Layout/Layout';
 
 function App() {
   return (
